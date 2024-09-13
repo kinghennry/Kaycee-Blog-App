dc6403f5fe50f0e5494994974886315befb9a6fb
kinghennry <keelechiogbu@gmail.com>
2024-09-13 13:59:18 +0100

    first commit and tailwind setup

---
+++ Frontend/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ Frontend/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+function SignUp() {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline text-red-500'>SignUp!</h1>
+    </div>
+  )
+}
+
+export default SignUp
