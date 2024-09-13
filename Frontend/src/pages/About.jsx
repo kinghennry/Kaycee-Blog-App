01d8f2d49a309f1bd1aa681b33e4b0e35d298cef
kinghennry <keelechiogbu@gmail.com>
2024-09-13 14:26:13 +0100

    fixed about bug

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -1,7 +1,7 @@
 function About() {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline text-red-500'>About!</h1>
+      <h1 className='text-3xl font-bold underline text-red-500'>About su!</h1>
     </div>
   )
 }
